23607ac9f071c64e57dec2f5208c0677c45de057
2014BDuck <2014bduck@gmail.com>
2020-10-11 14:32:35 +0800

    Added grpc gateway

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/2014BDuck/tag-service
 go 1.15
 
 require (
-	github.com/fullstorydev/grpcurl v1.7.0 // indirect
 	github.com/golang/protobuf v1.4.2
+	github.com/grpc-ecosystem/grpc-gateway v1.14.4
+	github.com/soheilhy/cmux v0.1.4
 	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
 	golang.org/x/sys v0.0.0-20201005172224-997123666555 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20201006033701-bcad7cf615f2 // indirect
-	google.golang.org/grpc v1.30.0
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/genproto v0.0.0-20201009135657-4d944d34d83c // indirect
+	google.golang.org/grpc v1.31.0
+	google.golang.org/grpc/examples v0.0.0-20201010204749-3c400e7fcc87 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,62 +1,103 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	pb "github.com/2014BDuck/tag-service/proto"
 	"github.com/2014BDuck/tag-service/server"
-	"github.com/soheilhy/cmux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"log"
-	"net"
 	"net/http"
+	"strings"
 )
 
 var port string
+
+type httpError struct {
+	Code    int32  `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func grpcGatewayError(ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, _ *http.Request, err error) {
+	s, ok := status.FromError(err)
+	if !ok {
+		s = status.New(codes.Unknown, err.Error())
+	}
+
+	httpError := httpError{Code: int32(s.Code()), Message: s.Message()}
+	details := s.Details()
+	for _, detail := range details {
+		if v, ok := detail.(*pb.Error); ok {
+			httpError.Code = v.Code
+			httpError.Message = v.Message
+		}
+	}
+
+	resp, _ := json.Marshal(httpError)
+	w.Header().Set("Content-type", marshaler.ContentType())
+	w.WriteHeader(runtime.HTTPStatusFromCode(s.Code()))
+	_, _ = w.Write(resp)
+}
 
 func init() {
 	flag.StringVar(&port, "port", "8002", "启动端口号")
 	flag.Parse()
 }
 
-func RunTCPServer(port string) (net.Listener, error) {
-	return net.Listen("tcp", ":"+port)
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
 }
 
-func RunHttpServer(port string) *http.Server {
-	serverMux := http.NewServeMux()
-	serverMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+func RunServer(port string) error {
+	httpMux := runHttpServer()
+	grpcS := runGrpcServer()
+	gatewayMux := runGrpcGatewayServer()
+	httpMux.Handle("/", gatewayMux)
+
+	return http.ListenAndServe(":"+port, grpcHandlerFunc(grpcS, httpMux))
+}
+
+func runHttpServer() *http.ServeMux {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte(`pong`))
 	})
-	return &http.Server{
-		Addr:    ":" + port,
-		Handler: serverMux,
-	}
+
+	return serveMux
 }
 
-func RunGrpcServer(port string) *grpc.Server {
+func runGrpcServer() *grpc.Server {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
 	return s
 }
 
+func runGrpcGatewayServer() *runtime.ServeMux {
+	endpoint := "0.0.0.0:" + port
+	runtime.HTTPError = grpcGatewayError
+	gwmux := runtime.NewServeMux()
+	dopts := []grpc.DialOption{grpc.WithInsecure()}
+	_ = pb.RegisterTagServiceHandlerFromEndpoint(context.Background(), gwmux, endpoint, dopts)
+	return gwmux
+}
+
 func main() {
-	l, err := RunTCPServer(port)
-	if err != nil {
-		log.Fatalf("Run TCPServer err: %v", err)
-	}
-	m := cmux.New(l)
-	grpcL := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldPrefixSendSettings("content-type", "application/grpc"))
-	httpL := m.Match(cmux.HTTP1Fast())
+	err := RunServer(port)
 
-	grpcS := RunGrpcServer(port)
-	httpS := RunHttpServer(port)
-
-	go grpcS.Serve(grpcL)
-	go httpS.Serve(httpL)
-
-	err = m.Serve()
 	if err != nil {
 		log.Fatalf("Run Server err: %v", err)
 	}
