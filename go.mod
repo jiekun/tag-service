67cd91dad0ff12d8949aef1152ce79cb16ae35b4
2014BDuck <2014bduck@gmail.com>
2020-10-11 12:58:02 +0800

    Added same port support for grpchttp

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/fullstorydev/grpcurl v1.7.0 // indirect
 	github.com/golang/protobuf v1.4.2
-	golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c // indirect
+	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
 	golang.org/x/sys v0.0.0-20201005172224-997123666555 // indirect
 	golang.org/x/text v0.3.3 // indirect
 	google.golang.org/genproto v0.0.0-20201006033701-bcad7cf615f2 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,63 @@
 package main
 
 import (
+	"flag"
 	pb "github.com/2014BDuck/tag-service/proto"
 	"github.com/2014BDuck/tag-service/server"
+	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 )
 
-var port = "8002"
+var port string
 
-func main() {
+func init() {
+	flag.StringVar(&port, "port", "8002", "启动端口号")
+	flag.Parse()
+}
+
+func RunTCPServer(port string) (net.Listener, error) {
+	return net.Listen("tcp", ":"+port)
+}
+
+func RunHttpServer(port string) *http.Server {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: serverMux,
+	}
+}
+
+func RunGrpcServer(port string) *grpc.Server {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
-	lis, err := net.Listen("tcp", ":"+port)
+	return s
+}
+
+func main() {
+	l, err := RunTCPServer(port)
 	if err != nil {
-		log.Fatalf("net.Listen err: %v", err)
+		log.Fatalf("Run TCPServer err: %v", err)
 	}
-	log.Printf("Listening: %s", port)
-	err = s.Serve(lis)
+	m := cmux.New(l)
+	grpcL := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldPrefixSendSettings("content-type", "application/grpc"))
+	httpL := m.Match(cmux.HTTP1Fast())
+
+	grpcS := RunGrpcServer(port)
+	httpS := RunHttpServer(port)
+
+	go grpcS.Serve(grpcL)
+	go httpS.Serve(httpL)
+
+	err = m.Serve()
 	if err != nil {
-		log.Fatalf("server.Server err: %v", err)
+		log.Fatalf("Run Server err: %v", err)
 	}
 }
